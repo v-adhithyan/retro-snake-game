f780773c84081136627ec6c1731a8583e28c691c
v-adhithyan <aavispeaks@gmail.com>
2019-07-22 07:18:42 +0530

    Add edge detection

--- snake.py
+++ snake.py
@@ -5,7 +5,6 @@ import random
 begin_x = 20
 begin_y = 7
 
-
 KEY_Q = 113
 KEY_UP = 65
 KEY_DOWN = 66
@@ -14,6 +13,7 @@ KEY_LEFT = 68
 SNAKE = '.'
 SNAKE_X, SNAKE_Y = 0, 0
 SCORE = 0
+DIRECTION = 'right'
 
 
 # Todo: Command pattern is suitable here, let's use that after game just works
@@ -22,6 +22,8 @@ SCORE = 0
 # Todo: screen edge detection
 # Todo: auto move snake
 # Todo: increase length of snake as per score
+# Todo: add best score
+
 def quit_game(screen):
     curses.nocbreak()
     screen.keypad(False)
@@ -29,30 +31,60 @@ def quit_game(screen):
     curses.endwin()
 
 
+def detect_screen_edge(func):
+
+    @functools.wraps(func)
+    def detector(*args, **kwargs):
+        screen = args[0]
+        cursor_x = kwargs.get('cursor_x')
+        cursor_y = kwargs.get('cursor_y')
+        height, width = screen.getmaxyx()
+
+        if cursor_y == 0:
+            cursor_y = height - 2
+        if cursor_y == height - 1:
+            cursor_y = 1
+        if cursor_x == 0:
+            cursor_x = width - 2
+        if cursor_x == width - 1:
+            cursor_x = 1
+
+        kwargs['cursor_x'] = cursor_x
+        kwargs['cursor_y'] = cursor_y
+
+        return func(*args, **kwargs)
+
+    return detector
+
+
 def prepare_food(screen, x, y, refresh=True):
-    screen.addstr(y, x, '0', curses.color_pair(2))
+    screen.addstr(y, x, '@', curses.color_pair(2))
     if refresh:
         screen.refresh()
 
 
+@detect_screen_edge
 def up(screen, **kwargs):
     cursor_y = kwargs.pop('cursor_y') - 1
     cursor_x = kwargs.pop('cursor_x')
     return add_snake(screen, cursor_y=cursor_y, cursor_x=cursor_x, direction='up')
 
 
+@detect_screen_edge
 def down(screen, **kwargs):
     cursor_y = kwargs.pop('cursor_y') + 1
     cursor_x = kwargs.pop('cursor_x')
     return add_snake(screen, cursor_y=cursor_y, cursor_x=cursor_x, direction='down')
 
 
+@detect_screen_edge
 def left(screen, **kwargs):
     cursor_y = kwargs.pop('cursor_y')
     cursor_x = kwargs.pop('cursor_x') - 1
     return add_snake(screen, cursor_y=cursor_y, cursor_x=cursor_x, direction='left')
 
 
+@detect_screen_edge
 def right(screen, **kwargs):
     cursor_y = kwargs.pop('cursor_y')
     cursor_x = kwargs.pop('cursor_x') + 1
@@ -60,7 +92,7 @@ def right(screen, **kwargs):
 
 
 def do_nothing(screen, **kwargs):
-    return kwargs.pop('cursor_x'), kwargs.pop('cursor_y')
+    return kwargs.pop('cursor_x'), kwargs.pop('cursor_y'), kwargs.pop('direction')
 
 
 def ate_food(f):
@@ -73,7 +105,7 @@ def ate_food(f):
             SNAKE_X = random.randint(0, width)
             SNAKE_Y = random.randint(0, height)
             SCORE = SCORE + 1
-            #SNAKE = '.' * (SCORE + 1)
+            # SNAKE = '.' * (SCORE + 1)
             prepare_food(screen, SNAKE_X, SNAKE_Y)
         return f(*args, **kwargs)
 
@@ -100,7 +132,7 @@ def add_snake(screen, **kwargs):
             #cursor_y = cursor_y - 2 if direction == 'down' else cursor_y - 1"""
     screen.addstr(cursor_y, cursor_x, SNAKE, curses.color_pair(1))
     screen.move(cursor_y, cursor_x)
-    return cursor_x, cursor_y
+    return cursor_x, cursor_y, direction
 
 
 def render_status_bar(screen, height, width):
@@ -112,8 +144,20 @@ def render_status_bar(screen, height, width):
     screen.attroff(curses.color_pair(3))
 
 
+def automove(screen, direction, cursor_x, cursor_y):
+    if direction == 'right':
+        return right(screen, cursor_x=cursor_x, cursor_y=cursor_y, direction=direction)
+    if direction == 'left':
+        return left(screen, cursor_x=cursor_x, cursor_y=cursor_y, direction=direction)
+    if direction == 'up':
+        return up(screen, cursor_x=cursor_x, cursor_y=cursor_y, direction=direction)
+    if direction == 'down':
+        return down(screen, cursor_x=cursor_x, cursor_y=cursor_y, direction=direction)
+
+
 def main(win):
     global SNAKE_X, SNAKE_Y, SCORE
+    direction = 'right'
     move_snake = dict()
     move_snake[KEY_UP] = up
     move_snake[KEY_DOWN] = down
@@ -129,15 +173,13 @@ def main(win):
     curses.noecho()
     curses.cbreak()
     screen.keypad(True)
-    win = curses.newwin(height//2, width//2, height//2, width//2)
+    win = curses.newwin(height // 2, width // 2, height // 2, width // 2)
     cursor_x = width // 2
     cursor_y = height // 2
-    screen.addstr("RETRO SNAKE GAME\n")
-    screen.addstr("Press Q to quit. Press arrow keys to play.\n\n")
     SNAKE_X = random.randint(0, width)
-    SNAKE_Y = random.randint(0, height)
+    SNAKE_Y = random.randint(0, height - 1)
     prepare_food(screen, x=SNAKE_X, y=SNAKE_Y, refresh=False)
-    cursor_x, cursor_y = add_snake(screen, cursor_x=cursor_x+1, cursor_y=cursor_y, direction='right')
+    cursor_x, cursor_y, direction = add_snake(screen, cursor_x=cursor_x + 1, cursor_y=cursor_y, direction='right')
     screen.refresh()
     while True:
         try:
@@ -149,7 +191,9 @@ def main(win):
                 screen.erase()
                 render_status_bar(screen, height, width)
                 prepare_food(screen, SNAKE_X, SNAKE_Y, refresh=False)
-                cursor_x, cursor_y = move_snake.get(key, do_nothing)(screen, cursor_x=cursor_x, cursor_y=cursor_y)
+                cursor_x, cursor_y, direction = move_snake.get(key, do_nothing)(screen, cursor_x=cursor_x,
+                                                                                cursor_y=cursor_y, direction=direction)
+            #automove(screen, direction, cursor_x, cursor_y)
             screen.refresh()
         except Exception as e:
             print(e.__repr__())
